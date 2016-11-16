27fb9304877da8bd251956ce930f0236c87b9ffb
unknown <ausbla01@U201184.usa.Arm.com>
2016-11-16 11:53:38 -0600

    grap port from env variable

--- app.py
+++ app.py
@@ -126,4 +126,5 @@ if __name__ == "__main__":
 	connector.deleteAllSubscriptions()							# remove all subscriptions, start fresh
 	connector.startLongPolling()								# start long polling connector.mbed.com
 	connector.setHandler('notifications', notificationHandler) 	# send 'notifications' to the notificationHandler FN
-	socketio.run(app,host='0.0.0.0', port=80)
+	port = int(os.environ.get("PORT", 8080))
+	socketio.run(app,host='0.0.0.0', port=port)
